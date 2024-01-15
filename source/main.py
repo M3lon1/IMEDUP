9cd379abe70e703affb1168b5255ac65a3c8b0b5
M3lon1 <sebastian.heckner@posteo.de>
2024-01-15 11:17:07 +0100

    added pipeline picture

--- source/main.py
+++ source/main.py
@@ -2,6 +2,9 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
+    # input picture
+    # rotate picture m times, around angle phi. output: collection of pictures
+    # calculate hash value of each rotated picture
+    # store each hash value in a database
+    # calculate distance between all pictures
---
+++ source/modules.py
@@ -0,0 +1,30 @@
+
+def get_picture():
+    """
+    get picture from some form of cloud based storage
+
+    :return:
+    """
+    pass
+
+
+def rotate_picture(p: complex, m: int, phi: int) -> list:
+    """
+    rotate picture p, m times around angle phi.
+    store each rotated picture in a list
+
+    :param p: input picture
+    :param m: how many rotations should be applied
+    :param phi: angle
+    :return: list of pictures
+    """
+    pass
+
+
+def calculate_hash(p: complex) -> hex:
+    """
+
+    :param p: picture
+    :return: hash value in hex
+    """
+    pass
