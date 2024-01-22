bddc4edea4abc3e3810c13e374b58fef778daed4
M3lon1 <sebastian.heckner@posteo.de>
2024-01-22 17:33:18 +0100

    added db file

--- source/db.py
+++ source/db.py
@@ -2,14 +2,25 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+
+cluster_name = "cluster0"
+username = "sebastianheckner1995"
+password = "jQpPmqIEHV6PPhim"
 uri = "mongodb+srv://sebastianheckner1995:jQpPmqIEHV6PPhim@cluster0.ej5xn85.mongodb.net/"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
+# create database
+database = client['imedup']
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+
+    # This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    # Get the database
+    print(database)
